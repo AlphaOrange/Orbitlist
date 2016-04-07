0531236edeade96ee805cd12b7fb9f0492a82ff7
Stefan <stefan@alphaorange.de>
2016-04-07 20:20:44 +0200

    Consistent terminology
    Entire code reworked for consistent naming: the whole list is called
    orbitlist, the lowest ul element the core, li are satellites, all
    satellites of the same height (depth in the original list) create an
    orbit. Closes #9
    Some translations into english for #11.

--- orbitlist.js
+++ orbitlist.js
@@ -1,181 +1,201 @@
-// jQuery um reverse() erweitern
+// jQuery extension: reversion jQuery element order
 jQuery.fn.reverse = [].reverse;
 
-// Default für Innen- und Außenradius (von 0 bis 1)
-var defaultInner = 0;       // Inneres Limit für Radius
-var defaultOuter = 1;       // Äußeres Limit für Radius
-var defaultBorders = 1;     // Raum zwischen Limit und innerem/äußerem Level
 
-// TODO2: nicht implementiert
+// Default values for inner and outer radius limits (between 0 and 1)
+var defaultInner = 0;       // Inner limit for radius
+var defaultOuter = 1;       // Outer limit for radius
+var defaultBorders = 1;     // Factor for space between limit
+                            // and inner/outer orbit
+
+// TODO: not yet implemented: Min density of satellites
 var minDensity = 8;
-var minDensityLevel1 = 4; // minimale density auf Level 1 - bei Unterschreiten asymmetrisch
+var minDensityLevel1 = 4;   // min density for 1st orbit
+                            // Orbitlist becomes asymetric if less satellites
 
-function $orbitlistJS_trace(knot) {
-    while (knot.length) {
-        knot.addClass('orbitlistJS-trace');
-        knot = knot.data('parent');
+
+// Trace satellite back to root
+function $orbitlistJS_trace(satellite) {
+    while (satellite.length) {
+        satellite.addClass('orbitlistJS-trace');
+        satellite = satellite.data('parent');
     }
 }
 
-function $orbitlistJS_ironOrbit(orbit) {
 
-    // Tiefe des Orbits im Dokument ermitteln
-    var baseDepth = orbit.parents().length;
+// Flatten Orbitlist HTML to one level only
+function $orbitlistJS_flatten(core) {
+
+    // Höhe der Orbitlist im Dokument ermitteln
+    var baseHeight = core.parents().length;
     
-    // Variable für Orbittiefe, wird weiter unten überschrieben
-    var orbitDepth = 1;
+    // Variable für höchsten Orbit
+    var orbitHeight;
 
-    // Alle Knoten: Elternknoten speichern, dann auf oberste Ebene verschieben
-    orbit.find('li').reverse().each(function () {
+    // Alle Satelliten: Elternelement speichern, dann auf oberste Ebene verschieben
+    core.find('li').reverse().each(function() {
         
-        var knot = $(this);
+        var satellite = $(this);
         
-        // Tiefe auswerten und css-Klasse anlegen
-        var depth = (knot.parents().length - baseDepth + 1) / 2;
-        knot.addClass('orbitlistJS-orbit-' + depth);
-        knot.data('depth', depth);
-        orbitDepth = Math.max(orbitDepth, depth);
+        // Höhe auswerten und css-Klasse anlegen
+        var height = (satellite.parents().length - baseHeight + 1) / 2;
+        satellite.addClass('orbitlistJS-orbit-' + height);
+        satellite.data('height', height);
+        orbitHeight = Math.max(orbitHeight, height);
 
-        // Elternknoten speichern sofern vorhanden
-        knot.data('parent', knot.parent().parent().filter(".orbit li"));
-        orbit.prepend(knot);
+        // Verweis auf Elternelement speichern sofern vorhanden
+        satellite.data('parent', satellite.parent().parent().filter(".orbit li"));
+        core.prepend(satellite);
                 
     });
     
-    // Tiefe des Orbits und Tiefe im Dokument in Orbitobjekt speichern
-    orbit.data('baseDepth', baseDepth);
-    orbit.data('orbitDepth', orbitDepth);
-    orbit.data('visibleDepth', 1);
+    // Höhe des Orbits und Höhe im Dokument in Core speichern
+    core.data('baseHeight', baseHeight);
+    core.data('orbitHeight', orbitHeight);
     
-    // Alle nun leeren Unterlisten löschen
-    orbit.find('ul').remove();
+    // Aktuell sichtbare Höhe ist initial 1
+    core.data('visibleHeight', 1);
+    
+    // Alle (nun leeren) Unterlisten löschen
+    core.find('ul').remove();
 
 }
 
-function $orbitlistJS_updateOrbit(orbit) {
+function $orbitlistJS_update(core) {
 
-    var density;
-    var angle;
+    var density;    // Density of satellites shown
+    var angle;      // Starting angle
 
     // Höhe/Breite des umgebenden Elements ermitteln
-    var frameW = orbit.parent().width();
-    var frameH = orbit.parent().height();
-    var radius = Math.min(frameW, frameH);
-    var offsetTop = (frameH - radius) / 2;
-    var offsetLeft = (frameW - radius) / 2;
+    var frameW = core.parent().width();
+    var frameH = core.parent().height();
+    var radius = Math.min(frameW, frameH) / 2;
+    var offsetTop = frameH / 2 - radius;
+    var offsetLeft = frameW / 2 - radius;
     
-    // Initiale Knoten
-    var knotDepth = 1;
-    var knots = orbit.find('.orbitlistJS-orbit-1');
+    // Erster/niedrigster Orbit
+    var orbitHeight = 1;
+    var orbit = core.find('.orbitlistJS-orbit-1');
 
-    var borders = orbit.data('orbitlistjs-borders');
-    var inner = orbit.data('orbitlistjs-inner');
-    var outer = orbit.data('orbitlistjs-outer');
-    var visibleDepth = orbit.data('visibleDepth');
+    // Eigenschaften der Orbitlist lesen
+    var borders = core.data('orbitlistjs-borders');
+    var inner = core.data('orbitlistjs-inner');
+    var outer = core.data('orbitlistjs-outer');
+    var visibleHeight = core.data('visibleHeight');
 
-    // Alle Ebenen formatieren
+    // Alle sichtbaren Orbits formatieren
     do {
 
-        // Dichte und Winkel der Knoten ermitteln
-        if (knotDepth === 1) {
-            density = knots.length;
+        // Dichte und Winkel der Satelliten ermitteln
+        if (orbitHeight === 1) {
+            density = orbit.length;
             angle = 0;
         } else {
-            var squeeze = 3;
-            density = Math.max((knots.length - 1) * squeeze, density); // density mindestens so hoch wie auf höheren Levels
-            angle = knots.first().data('parent').data('angle') - 1/(density/(knots.length-1))/2;
+            var squeeze = 3;    // TODO: zu variablem Parameter ändern
+            // density mindestens so hoch wie im niedrigeren Orbit
+            density = Math.max((orbit.length - 1) * squeeze, density);
+            angle = orbit.first().data('parent').data('angle') - 1/(density/(orbit.length-1))/2;
         }
 
-        // Alle Knoten formatieren
-        knots.each(function (index) {
+        // Alle Satelliten formatieren
+        orbit.each(function(index) {
 
             // TODO: Zur Verständlichkeit Rechnung aufteilen
-            var knot = $(this);
-            var distance = (visibleDepth === 1 ? 0.5 : (borders + knotDepth - 1) / (2 * borders + visibleDepth - 1));
+            var satellite = $(this);
+            var distance = (visibleHeight === 1 ? 0.5 : (borders + orbitHeight - 1) / (2 * borders + visibleHeight - 1));
             distance = inner + distance * (outer - inner);
 
-            var posTop = radius * distance / 2 * (-Math.cos((index / density + angle) * Math.PI * 2)) + radius / 2 + orbit.parent().offset().top + offsetTop - knot.height() / 2;
-            var posLeft = radius * distance / 2 * (Math.sin((index / density + angle) * Math.PI * 2)) + radius / 2 + orbit.parent().offset().left + offsetLeft - knot.width() / 2;
-            knot.offset({top: posTop, left: posLeft});
+            // Position satellite
+            var posTop = radius * distance * (-Math.cos((index / density + angle) * Math.PI * 2)) + radius + core.parent().offset().top + offsetTop - satellite.height() / 2;
+            var posLeft = radius * distance * (Math.sin((index / density + angle) * Math.PI * 2)) + radius + core.parent().offset().left + offsetLeft - satellite.width() / 2;
+            satellite.offset({top: posTop, left: posLeft});
             
-            // Winkel für Kindknoten speichern
-            knot.data('angle', index / density + angle);
+            // Winkel für Kindsatelliten speichern
+            satellite.data('angle', index / density + angle);
+            
         });
         
-        // Eine Ebene tiefer steigen
-        knotDepth++;
-        knots = orbit.find('.orbitlistJS-orbit-' + knotDepth + ':visible');
+        // Eine Ebene höher steigen
+        orbitHeight++;
+        orbit = core.find('.orbitlistJS-orbit-' + orbitHeight + ':visible');
         
-    } while (knots.length);
+    } while (orbit.length);
 
 }
 
+
+// Document ready setup
 $(function () {
 
-    // Jede Liste mit .orbit umwandeln
-    $('ul.orbit').each(function (index) {
+    // Jede Liste mit Klasse .orbit in Orbitlist umwandeln
+    $('ul.orbit').each(function(index) {
 
-        // Orbit-Objekt erstellen
-        var orbit = $(this);
+        // Orbitlist Core erstellen
+        var core = $(this);
 
         // Klasse für CSS einfügen
-        orbit.addClass('orbitlistJS');
+        core.addClass('orbitlistJS');
 
-        // Daten für Orbit festlegen
-        if (orbit.data('orbitlistjs-inner') === undefined) { orbit.data('orbitlistjs-inner', defaultInner); }
-        if (orbit.data('orbitlistjs-outer') === undefined) { orbit.data('orbitlistjs-outer', defaultOuter); }        
-        if (orbit.data('orbitlistjs-borders') === undefined) { orbit.data('orbitlistjs-borders', defaultBorders); }
+        // Eigenschaften für Orbitlist festlegen
+        if (core.data('orbitlistjs-inner') === undefined)   { core.data('orbitlistjs-inner', defaultInner); }
+        if (core.data('orbitlistjs-outer') === undefined)   { core.data('orbitlistjs-outer', defaultOuter); }        
+        if (core.data('orbitlistjs-borders') === undefined) { core.data('orbitlistjs-borders', defaultBorders); }
 
-        // Orbit-Hierarchie auf eine Ebene umordnen, um Abhängigkeiten
-        // für absolute Positionierung zu lösen
-        $orbitlistJS_ironOrbit(orbit);
+        // HTML-Listen auf eine Ebene reduzieren
+        // Andernfalls Probleme beim verschieben von absolut positionierten
+        // Elementen wegen Abhängigkeiten
+        $orbitlistJS_flatten(core);
 
-        // Alle außer Ebene 1 ausblenden
-        orbit.find('li').filter(function() { 
-            return $(this).data("depth") > 1;
+        // Alle Orbits außer den ersten ausblenden
+        core.find('li').filter(function() { 
+            return $(this).data('height') > 1;
         }).hide();
 
-// TODO: Hier wird noch zu viel doppelt und dreifach geändert - besser filtern!
-        // Click-Events binden
-        orbit.find('li').click(function(event) {
-            
-            knot = $(this);
+        // TODO: Hier wird noch zu viel doppelt und dreifach geändert,
+        // ein- und ausgeblendet - besser filtern!
 
-            // Knotenklassen neu verteilen
-            if (knot.hasClass('orbitlistJS-active')) {
-                knot.removeClass('orbitlistJS-active orbitlistJS-trace');
-                knot.data('parent').addClass('orbitlistJS-active');
+        // Click-Events an Satelliten binden binden
+        // TODO: nur an Satelliten mit Kindern binden, dazu
+        // isParent-Eigenschaft implementieren
+        core.find('li').click(function(event) {
+            
+            satellite = $(this);
+
+            // Satellitenklassen neu verteilen
+            if (satellite.hasClass('orbitlistJS-active')) {
+                satellite.removeClass('orbitlistJS-active orbitlistJS-trace');
+                satellite.data('parent').addClass('orbitlistJS-active');
             } else {
-                orbit.find('li').removeClass('orbitlistJS-active orbitlistJS-trace');
-                knot.addClass('orbitlistJS-active');
-                $orbitlistJS_trace(knot);
+                core.find('li').removeClass('orbitlistJS-active orbitlistJS-trace');
+                satellite.addClass('orbitlistJS-active');
+                $orbitlistJS_trace(satellite);
             }
             
-            // Nur alle Knoten anzeigen, die kein Parent haben oder ein Parent im Trace
+            // Nur Knoten anzeigen, die kein Parent haben oder ein Parent im Trace
             // Außerdem aktuell angezeigte Tiefe berechnen
-            var visibleDepth = 1;
-            orbit.find('li').hide();
-            orbit.find('li').filter(function(index) {
+            var visibleHeight = 1;
+            core.find('li').hide();
+            core.find('li').filter(function(index) {
                 var parent = $(this).data('parent');
-                var showKnot = !parent.length | parent.hasClass('orbitlistJS-trace');
-                if (showKnot) { visibleDepth = Math.max(visibleDepth, $(this).data('depth')); }
-                return showKnot;
+                var showSatellite = !parent.length | parent.hasClass('orbitlistJS-trace');
+                if (showSatellite) { visibleHeight = Math.max(visibleHeight, $(this).data('height')); }
+                return showSatellite;
             }).show();
-            orbit.data('visibleDepth', visibleDepth);
+            core.data('visibleHeight', visibleHeight);
             
-            // Orbit updaten
-            $orbitlistJS_updateOrbit(orbit);
+            // Orbitlist updaten
+            $orbitlistJS_update(core);
             
             // Kein Bubbling für Click-Event
             event.stopPropagation();
         });
 
-        // Orbit updaten
-        $orbitlistJS_updateOrbit(orbit);
+        // Orbitlist updaten für Startansicht
+        $orbitlistJS_update(core);
 
-        // Auch bei resize Orbits neu berechnen
+        // Auch bei resize Orbitlist neu berechnen
         $(window).resize(function() {
-            $orbitlistJS_updateOrbit(orbit);
+            $orbitlistJS_update(core);
         });
 
     });
